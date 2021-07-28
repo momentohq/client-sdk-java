6a18f3e309342b25eb292ec0fdcf82ec8dc12a60
Matt Straathof <matt.straat@gmail.com>
2021-07-28 12:13:25 -0700

    feat: add maven publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,13 +2,46 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     `java-library`
+    `maven-publish`
     idea
     java
 }
 
+group = "org.momento"
+version = findProperty("version") as String
+
+var awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID") ?: findProperty("aws_access_key_id") as String? ?: ""
+var awsSecretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY") ?: findProperty("aws_secret_access_key") as String? ?: ""
+
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    maven {
+        name = ""
+        url = uri("s3://artifact-814370081888-us-west-2/client-sdk-java/release")
+        credentials(AwsCredentials::class) {
+            accessKey = awsAccessKeyId
+            secretKey = awsSecretAccessKey
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("myLibrary") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("s3://artifact-814370081888-us-west-2/client-sdk-java/release")
+            credentials(AwsCredentials::class) {
+                accessKey = awsAccessKeyId
+                secretKey = awsSecretAccessKey
+            }
+        }
+    }
 }
 
 java {
