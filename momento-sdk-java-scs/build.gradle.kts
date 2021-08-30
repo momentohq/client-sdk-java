125f8e508207b7dd791516222c4b7e718d8e7935
Chris Price <chris@momentohq.com>
2021-08-30 14:32:00 -0700

    chore(#13): Split integration tests out to separate build task
    The default build shouldn't have dependencies on auth tokens
    or network connections.  Previously the tests we had in the
    unit test area of the build had these dependencies, so the
    build couldn't be run without setting them up.
    
    This commit moves the integration tests to a separate build
    task, `integrationTest`.  The main build can now be run
    indendently.
    
    CI/CD jobs are updated accordingly.

--- momento-sdk-java-scs/build.gradle.kts
+++ momento-sdk-java-scs/build.gradle.kts
@@ -71,3 +71,37 @@ tasks.test {
         events("passed", "skipped", "failed")
     }
 }
+
+
+// Set up integration testing -------
+
+// https://docs.gradle.org/current/userguide/java_testing.html#sec:configuring_java_integration_tests
+
+// Define independent source set so we can run separately then unit tests
+sourceSets {
+    create("intTest") {
+        compileClasspath += main.get().output + configurations.testRuntimeClasspath
+        runtimeClasspath += output + compileClasspath
+    }
+}
+
+// Extend base project run time and test configuration then add integration specific deps
+val intTestImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+configurations["intTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
+
+// Define separate task for running int tests
+val integrationTest = task<Test>("integrationTest") {
+    description = "Runs the integration tests"
+    group = "verification"
+    testClassesDirs = sourceSets["intTest"].output.classesDirs
+    classpath = sourceSets["intTest"].runtimeClasspath
+    useJUnitPlatform()
+    testLogging {
+//         showStandardStreams = true  // Un comment this if need full integration test output for stdout & stderr
+        exceptionFormat = TestExceptionFormat.FULL
+        events("passed", "skipped", "failed")
+    }
+    outputs.upToDateWhen { false }
+}
