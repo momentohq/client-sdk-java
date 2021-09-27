bc87310bd2c5fea3ebfb32f19d7d52a7a413ef44
eaddingtonwhite <5491827+ellery44@users.noreply.github.com>
2021-09-27 10:07:24 -0700

    feat: adds basic control plane impl

--- momento-sdk/src/main/java/momento/sdk/Momento.java
+++ momento-sdk/src/main/java/momento/sdk/Momento.java
@@ -1,13 +1,79 @@
 package momento.sdk;
 
-// TODO: Add Implementation and make non-abstract
-public abstract class Momento {
+import grpc.control_client.CreateCacheRequest;
+import grpc.control_client.CreateCacheResponse;
+import grpc.control_client.ScsControlGrpc;
+import grpc.control_client.ScsControlGrpc.ScsControlBlockingStub;
+import io.grpc.ClientInterceptor;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
 
-  public static Momento init(String authToken) {
-    throw new UnsupportedOperationException();
-  }
+import java.util.ArrayList;
+import java.util.List;
 
-  public abstract void createCache(String cacheName);
+public class Momento {
 
-  public abstract Cache getCache(String cacheName);
+    private static Momento momentoInstance = null;
+    private final String authToken;
+    private final ScsControlBlockingStub blockingStub;
+    private final ManagedChannel channel;
+
+    public static void init(String authToken) {
+        if (authToken == null){
+            throw new IllegalArgumentException("cannot pass a null authToken");
+        }
+        momentoInstance = new momento.sdk.Momento(authToken);
+    }
+
+    private Momento(String authToken) {
+        this.authToken = authToken;
+        // FIXME get endpoint from JWT claim
+        NettyChannelBuilder channelBuilder = NettyChannelBuilder.forAddress("FIXME", 443);
+        channelBuilder.useTransportSecurity();
+        channelBuilder.disableRetry();
+        List<ClientInterceptor> clientInterceptors = new ArrayList<>();
+        clientInterceptors.add(new AuthInterceptor(authToken));
+        channelBuilder.intercept(clientInterceptors);
+        ManagedChannel channel = channelBuilder.build();
+        this.blockingStub = ScsControlGrpc.newBlockingStub(channel);
+        this.channel = channel;
+    }
+
+
+    public static Cache createCache(String cacheName) {
+        checkInitialized();
+        checkCacheNameValid(cacheName);
+        CreateCacheResponse ignored = momentoInstance.blockingStub.createCache(buildCreateCacheRequest(cacheName));
+        return new Cache(momentoInstance.authToken, cacheName);
+    }
+
+    public Cache getCache(String cacheName) {
+        checkInitialized();
+        checkCacheNameValid(cacheName);
+        return new Cache(momentoInstance.authToken, cacheName);
+    }
+
+    private static CreateCacheRequest buildCreateCacheRequest(String cacheName) {
+        return CreateCacheRequest
+                .newBuilder()
+                .setCacheName(cacheName)
+                .build();
+    }
+
+    private static void checkInitialized(){
+        if (momentoInstance == null){
+            throw new RuntimeException("must initialize momento sdk with auth token");
+        }
+    }
+
+    private static void checkCacheNameValid(String cacheName){
+        if (cacheName == null){
+            throw new IllegalArgumentException("null cacheName passed");
+        }
+    }
+
+    public void close() throws InterruptedException {
+        this.channel.shutdown().awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
+    }
+
 }
