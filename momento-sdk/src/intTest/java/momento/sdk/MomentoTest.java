6fae24dffaffe2f0d464cb1b802464ca7344758f
gautamomento <89037104+gautamomento@users.noreply.github.com>
2021-10-06 22:37:12 -0700

    feat: Validations for authToken, key, value, ttl (#82)
    * feat: Validations for authToken, key, value, ttl
    
    * fix broken tests

--- momento-sdk/src/intTest/java/momento/sdk/MomentoTest.java
+++ momento-sdk/src/intTest/java/momento/sdk/MomentoTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import momento.sdk.exceptions.CacheAlreadyExistsException;
-import momento.sdk.exceptions.ClientSdkException;
 import momento.sdk.exceptions.InvalidArgumentException;
 import momento.sdk.messages.CacheGetResponse;
 import momento.sdk.messages.CacheSetResponse;
@@ -46,29 +45,14 @@ final class MomentoTest {
 
   @Test
   void testHappyPath() {
-    Momento momento = Momento.builder().authToken(authToken).build();
+    Momento momento = Momento.builder(authToken).build();
     runHappyPathTest(momento, cacheName);
-  }
-
-  @Test
-  void authTokenWithNoEndpointAndNoEndpointOverride_throwsException() {
-    assertThrows(
-        ClientSdkException.class,
-        () -> Momento.builder().authToken(TEST_AUTH_TOKEN_NO_ENDPOINT).build());
-  }
-
-  @Test
-  void missingAuthToken_throwsException() {
-    assertThrows(ClientSdkException.class, () -> Momento.builder().build());
   }
 
   @Test
   void testInvalidCacheName() {
     Momento momento =
-        Momento.builder()
-            .authToken(authToken)
-            .endpointOverride(DEFAULT_MOMENTO_HOSTED_ZONE_ENDPOINT)
-            .build();
+        Momento.builder(authToken).endpointOverride(DEFAULT_MOMENTO_HOSTED_ZONE_ENDPOINT).build();
 
     assertThrows(InvalidArgumentException.class, () -> getOrCreate(momento, "     "));
   }
@@ -76,10 +60,7 @@ final class MomentoTest {
   @Test
   void clientWithEndpointOverride_succeeds() {
     Momento momentoWithEndpointOverride =
-        Momento.builder()
-            .authToken(authToken)
-            .endpointOverride(DEFAULT_MOMENTO_HOSTED_ZONE_ENDPOINT)
-            .build();
+        Momento.builder(authToken).endpointOverride(DEFAULT_MOMENTO_HOSTED_ZONE_ENDPOINT).build();
     runHappyPathTest(momentoWithEndpointOverride, cacheName);
   }
 
