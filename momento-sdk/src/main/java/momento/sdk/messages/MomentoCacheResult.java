3383de4c2ea37100b72380a40ac48b69b2ae4f31
gautamomento <gautam@momentohq.com>
2021-10-21 10:48:45 -0700

    add reason for deprecation.

--- momento-sdk/src/main/java/momento/sdk/messages/MomentoCacheResult.java
+++ momento-sdk/src/main/java/momento/sdk/messages/MomentoCacheResult.java
@@ -4,6 +4,9 @@ import grpc.cache_client.ECacheResult;
 
 /** Encapsulates the status of the Cache operation */
 public enum MomentoCacheResult {
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   */
   @Deprecated
   Internal_Server_Error(ECacheResult.Internal_Server_Error),
   /** Status when set operation succeeds. */
@@ -12,12 +15,24 @@ public enum MomentoCacheResult {
   Hit(ECacheResult.Hit),
   /** Status if an item was not found in Cache. */
   Miss(ECacheResult.Miss),
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   */
   @Deprecated
   Bad_Request(ECacheResult.Bad_Request),
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   */
   @Deprecated
   Unauthorized(ECacheResult.Unauthorized),
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   */
   @Deprecated
   Service_Unavailable(ECacheResult.Service_Unavailable),
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   */
   @Deprecated
   Unknown(65535);
 
