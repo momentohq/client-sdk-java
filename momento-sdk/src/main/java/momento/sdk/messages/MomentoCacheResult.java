83c8af2cc2aaff285b769d8771a9d20ac2c985aa
gautamomento <gautam@momentohq.com>
2021-10-21 10:55:03 -0700

    make deprectaed comment more specific

--- momento-sdk/src/main/java/momento/sdk/messages/MomentoCacheResult.java
+++ momento-sdk/src/main/java/momento/sdk/messages/MomentoCacheResult.java
@@ -4,7 +4,11 @@ import grpc.cache_client.ECacheResult;
 
 /** Encapsulates the status of the Cache operation */
 public enum MomentoCacheResult {
-  /** @deprecated - Experimental and will be deprecated soon. */
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   *     <p>SDK will throw an appropriate sub-class of {@link
+   *     momento.sdk.exceptions.CacheServiceException} once the switch is done.
+   */
   @Deprecated
   Internal_Server_Error(ECacheResult.Internal_Server_Error),
   /** Status when set operation succeeds. */
@@ -13,16 +17,32 @@ public enum MomentoCacheResult {
   Hit(ECacheResult.Hit),
   /** Status if an item was not found in Cache. */
   Miss(ECacheResult.Miss),
-  /** @deprecated - Experimental and will be deprecated soon. */
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   *     <p>SDK will throw an appropriate sub-class of {@link
+   *     momento.sdk.exceptions.CacheServiceException} once the switch is done.
+   */
   @Deprecated
   Bad_Request(ECacheResult.Bad_Request),
-  /** @deprecated - Experimental and will be deprecated soon. */
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   *     <p>SDK will throw an appropriate sub-class of {@link
+   *     momento.sdk.exceptions.CacheServiceException} once the switch is done.
+   */
   @Deprecated
   Unauthorized(ECacheResult.Unauthorized),
-  /** @deprecated - Experimental and will be deprecated soon. */
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   *     <p>SDK will throw an appropriate sub-class of {@link
+   *     momento.sdk.exceptions.CacheServiceException} once the switch is done.
+   */
   @Deprecated
   Service_Unavailable(ECacheResult.Service_Unavailable),
-  /** @deprecated - Experimental and will be deprecated soon. */
+  /**
+   * @deprecated - Experimental and will be deprecated soon.
+   *     <p>SDK will throw an appropriate sub-class of {@link
+   *     momento.sdk.exceptions.CacheServiceException} once the switch is done.
+   */
   @Deprecated
   Unknown(65535);
 
