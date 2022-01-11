c311d2825177627ebd144a393fd57ec0b9dcb4b2
gautamomento <gautam@momentohq.com>
2022-01-11 11:16:49 -0800

    fix formatting

--- momento-sdk/src/main/java/momento/sdk/CacheClientBuilder.java
+++ momento-sdk/src/main/java/momento/sdk/CacheClientBuilder.java
@@ -1,6 +1,5 @@
 package momento.sdk;
 
-import momento.sdk.exceptions.CacheAlreadyExistsException;
 import momento.sdk.exceptions.CacheNotFoundException;
 import momento.sdk.exceptions.ClientSdkException;
 
@@ -20,8 +19,10 @@ public final class CacheClientBuilder {
 
   /** Signal the builder to create a new Cache if one with the given name doesn't exist. */
   // TODO: Should we drop this? The reason is as follows: (Will remove this after PR discussion)
-  // In normal course of operation, I do expect customers running production code to be able to just inject Momento in
-  // in their application and then just always do get a Cache entity (since it is cheap) and then perform operations.
+  // In normal course of operation, I do expect customers running production code to be able to just
+  // inject Momento in
+  // in their application and then just always do get a Cache entity (since it is cheap) and then
+  // perform operations.
   //
   // Following class is for demonstration purposes
   //
@@ -44,8 +45,10 @@ public final class CacheClientBuilder {
   //    }
   //  }
   //
-  // We would never want customers to use createCacheIfDoesntExist. Although it ensures that Cache will always be
-  // present, adding that option to production code will always result in a GET first before any other operation thus
+  // We would never want customers to use createCacheIfDoesntExist. Although it ensures that Cache
+  // will always be
+  // present, adding that option to production code will always result in a GET first before any
+  // other operation thus
   // incurring additional latency.
   //
   // The flipside to removing this would potentially require customers -
@@ -54,13 +57,16 @@ public final class CacheClientBuilder {
   // 3. Add custom code to catch NotFoundExceptions and then create cache
   // 4. It will endup making demonstration code longer.
   //
-  // An intermediate way may be to keep this method and add good comments around why this shouldn't be done in
+  // An intermediate way may be to keep this method and add good comments around why this shouldn't
+  // be done in
   // production code.
   //
   // Other option is to add this optional flag to get or set operations.
   //
-  // So, we take this parameter on the builder but we only create a cache if we encounter CacheNotFound on the requested
-  // operation. There is still some race so some calls may end up taking longer around the time cache doesn't exist. But
+  // So, we take this parameter on the builder but we only create a cache if we encounter
+  // CacheNotFound on the requested
+  // operation. There is still some race so some calls may end up taking longer around the time
+  // cache doesn't exist. But
   // will eventually even out. May also endup adding additional risk to control plan.
   //
   // e.g. code
