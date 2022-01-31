4326eb3094082c85a705949ec5c103518efae9a3
gautamomento <gautam@momentohq.com>
2022-01-31 15:19:38 -0800

    fix class comment

--- momento-sdk/src/main/java/momento/sdk/exceptions/AuthenticationException.java
+++ momento-sdk/src/main/java/momento/sdk/exceptions/AuthenticationException.java
@@ -1,6 +1,6 @@
 package momento.sdk.exceptions;
 
-/** Exception when provided token is not provided or is invalid. */
+/** Exception when token is not provided or is invalid. */
 public class AuthenticationException extends CacheServiceException {
   public AuthenticationException(String message) {
     super(message);
