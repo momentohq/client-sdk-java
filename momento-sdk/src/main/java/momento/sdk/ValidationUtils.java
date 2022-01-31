82b6dc1056917f258226ab4e42ed3f7c8759fa5f
gautamomento <gautam@momentohq.com>
2022-01-31 15:18:28 -0800

    fix case

--- momento-sdk/src/main/java/momento/sdk/ValidationUtils.java
+++ momento-sdk/src/main/java/momento/sdk/ValidationUtils.java
@@ -5,7 +5,7 @@ import momento.sdk.exceptions.InvalidArgumentException;
 final class ValidationUtils {
 
   static final String CACHE_ITEM_TTL_CANNOT_BE_NEGATIVE = "Cache item TTL cannot be negative.";
-  static final String A_NON_NULL_KEY_IS_REQUIRED = "A non-null Key is required.";
+  static final String A_NON_NULL_KEY_IS_REQUIRED = "A non-null key is required.";
   static final String A_NON_NULL_VALUE_IS_REQUIRED = "A non-null value is required.";
   static final String CACHE_NAME_IS_REQUIRED = "Cache name is required.";
 
