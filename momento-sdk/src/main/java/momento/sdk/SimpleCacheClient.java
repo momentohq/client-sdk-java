29d23f9aa0b0239b77e6421fa17897d3e85cc46c
Nate Anderson <nate@momentohq.com>
2023-03-06 17:49:05 -0800

    chore: update getCache response
    Update the response returned by the get and getAsync methods to match
    the pattern we have established in other SDKs. The response has three
    implementations: Hit, Miss, and Error, and it no longer throws. It is
    intended to be used with pattern matching.
    
    Change the deleteAsync method to delete instead of getting.
    
    Add a test dependency on AssertJ, a fluent assertion library.

--- momento-sdk/src/main/java/momento/sdk/SimpleCacheClient.java
+++ momento-sdk/src/main/java/momento/sdk/SimpleCacheClient.java
@@ -10,16 +10,7 @@ import momento.sdk.exceptions.AlreadyExistsException;
 import momento.sdk.exceptions.BadRequestException;
 import momento.sdk.exceptions.ClientSdkException;
 import momento.sdk.exceptions.NotFoundException;
-import momento.sdk.messages.CacheDeleteResponse;
-import momento.sdk.messages.CacheGetResponse;
-import momento.sdk.messages.CacheSetResponse;
-import momento.sdk.messages.CreateCacheResponse;
-import momento.sdk.messages.CreateSigningKeyResponse;
-import momento.sdk.messages.DeleteCacheResponse;
-import momento.sdk.messages.FlushCacheResponse;
-import momento.sdk.messages.ListCachesResponse;
-import momento.sdk.messages.ListSigningKeysResponse;
-import momento.sdk.messages.RevokeSigningKeyResponse;
+import momento.sdk.messages.*;
 
 /** Client to perform operations against the Simple Cache Service */
 public final class SimpleCacheClient implements Closeable {
