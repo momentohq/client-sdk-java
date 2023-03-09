f2a8e9f1414dee2505ebbe7bc67b49e5ea809feb
Nate Anderson <nate@momentohq.com>
2023-03-09 13:13:31 -0800

    remove extra getters from CacheGetResponse

--- momento-sdk/src/main/java/momento/sdk/messages/CacheGetResponse.java
+++ momento-sdk/src/main/java/momento/sdk/messages/CacheGetResponse.java
@@ -1,9 +1,6 @@
 package momento.sdk.messages;
 
 import com.google.protobuf.ByteString;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import momento.sdk.exceptions.SdkException;
 
@@ -27,40 +24,12 @@ public interface CacheGetResponse {
     }
 
     /**
-     * Gets the retrieved value as a {@link ByteBuffer}.
-     *
-     * @return the value.
-     */
-    public ByteBuffer valueByteBuffer() {
-      return value.asReadOnlyByteBuffer();
-    }
-
-    /**
      * Gets the retrieved value as a UTF-8 {@link String}
      *
      * @return the value.
      */
     public String valueString() {
-      return valueString(StandardCharsets.UTF_8);
-    }
-
-    /**
-     * Gets the retrieved value as a {@link String}.
-     *
-     * @param charset the string encoding to use.
-     * @return the value.
-     */
-    public String valueString(Charset charset) {
-      return value.toString(charset);
-    }
-
-    /**
-     * Gets the retrieved value as an {@link InputStream}.
-     *
-     * @return the value.
-     */
-    public InputStream valueInputStream() {
-      return value.newInput();
+      return value.toString(StandardCharsets.UTF_8);
     }
   }
 
