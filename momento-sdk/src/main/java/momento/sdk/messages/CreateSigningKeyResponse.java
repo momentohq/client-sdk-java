d051c2c11118926d9a775e80496698595d70679d
Nate Anderson <nate@momentohq.com>
2023-03-10 10:19:36 -0800

    feat: convert responses to new pattern part deux
    Convert CreateSigningKeyResponse, ListCachesResponse
    ListSigningKeysResponse, and RevokeSigningKeyResponse.
    
    Remove page tokens from list caches and list signing keys, following the
    precedent set in the other SDKs.

--- momento-sdk/src/main/java/momento/sdk/messages/CreateSigningKeyResponse.java
+++ momento-sdk/src/main/java/momento/sdk/messages/CreateSigningKeyResponse.java
@@ -1,33 +1,52 @@
 package momento.sdk.messages;
 
 import java.util.Date;
+import momento.sdk.exceptions.SdkException;
 
-public class CreateSigningKeyResponse {
-  public CreateSigningKeyResponse(String keyId, String endpoint, String key, Date expiresAt) {
-    this.keyId = keyId;
-    this.endpoint = endpoint;
-    this.key = key;
-    this.expiresAt = expiresAt;
+/** Response for a create signing key operation */
+public interface CreateSigningKeyResponse {
+
+  /** A successful create signing key operation. Contains the new signing key and metadata. */
+  class Success implements CreateSigningKeyResponse {
+
+    private final String keyId;
+    private final String endpoint;
+    private final String key;
+    private final Date expiresAt;
+
+    public Success(String keyId, String endpoint, String key, Date expiresAt) {
+      this.keyId = keyId;
+      this.endpoint = endpoint;
+      this.key = key;
+      this.expiresAt = expiresAt;
+    }
+
+    public String getKeyId() {
+      return keyId;
+    }
+
+    public String getEndpoint() {
+      return endpoint;
+    }
+
+    public String getKey() {
+      return key;
+    }
+
+    public Date getExpiresAt() {
+      return expiresAt;
+    }
   }
 
-  public String getKeyId() {
-    return keyId;
-  }
+  /**
+   * A failed signing key creation operation. The response itself is an exception, so it can be
+   * directly thrown, or the cause of the error can be retrieved with {@link #getCause()}. The
+   * message is a copy of the message of the cause.
+   */
+  class Error extends SdkException implements CreateSigningKeyResponse {
 
-  public String getEndpoint() {
-    return endpoint;
+    public Error(SdkException cause) {
+      super(cause.getMessage(), cause);
+    }
   }
-
-  public String getKey() {
-    return key;
-  }
-
-  public Date getExpiresAt() {
-    return expiresAt;
-  }
-
-  private final String keyId;
-  private final String endpoint;
-  private final String key;
-  private final Date expiresAt;
 }
