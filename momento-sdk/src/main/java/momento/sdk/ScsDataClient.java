2aba0adcbef03c9d8cf9d6e4a1b162fcfb2aa824
rishtigupta <rishti@momentohq.com>
2023-04-04 15:24:11 -0700

    feat: Change implementation from AbstractMap to Map

--- momento-sdk/src/main/java/momento/sdk/ScsDataClient.java
+++ momento-sdk/src/main/java/momento/sdk/ScsDataClient.java
@@ -64,7 +64,6 @@ import io.grpc.stub.MetadataUtils;
 import java.io.Closeable;
 import java.nio.ByteBuffer;
 import java.time.Duration;
-import java.util.AbstractMap;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
