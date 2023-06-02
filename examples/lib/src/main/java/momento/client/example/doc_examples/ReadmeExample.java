e3de1e2619294619d216dff83904131b0c5eb83b
Nate Anderson <nate@momentohq.com>
2023-06-02 16:08:10 -0700

    feat: Add example code snippets that will populate the dev docs (#287)
    Simplify readme and use a code snippet.
    
    Add code snippets for the cheat sheet and docs examples.

---
+++ examples/lib/src/main/java/momento/client/example/doc_examples/ReadmeExample.java
@@ -0,0 +1,29 @@
+package momento.client.example.doc_examples;
+
+import java.time.Duration;
+import momento.sdk.CacheClient;
+import momento.sdk.auth.CredentialProvider;
+import momento.sdk.config.Configurations;
+import momento.sdk.responses.cache.GetResponse;
+
+public class ReadmeExample {
+  public static void main(String[] args) {
+    try (final CacheClient cacheClient =
+        CacheClient.builder(
+                CredentialProvider.fromEnvVar("MOMENTO_AUTH_TOKEN"),
+                Configurations.Laptop.v1(),
+                Duration.ofSeconds(60))
+            .build()) {
+      final String cacheName = "cache";
+
+      cacheClient.createCache(cacheName).join();
+
+      cacheClient.set(cacheName, "foo", "bar").join();
+
+      final GetResponse response = cacheClient.get(cacheName, "foo").join();
+      if (response instanceof GetResponse.Hit hit) {
+        System.out.println("Got value: " + hit.valueString());
+      }
+    }
+  }
+}
