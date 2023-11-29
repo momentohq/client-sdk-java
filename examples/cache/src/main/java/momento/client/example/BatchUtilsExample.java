62132b93457a1535f9bb35e024517ec904ab3e5e
Pratik Agarwal <pratik@momentohq.com>
2023-11-29 22:38:44 +0530

    chore: address Michael's feedback

--- examples/cache/src/main/java/momento/client/example/BatchUtilsExample.java
+++ examples/cache/src/main/java/momento/client/example/BatchUtilsExample.java
@@ -1,7 +1,8 @@
 package momento.client.example;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import momento.sdk.CacheClient;
 import momento.sdk.auth.CredentialProvider;
 import momento.sdk.auth.EnvVarCredentialProvider;
@@ -32,6 +33,11 @@ public class BatchUtilsExample {
 
   private static final String CACHE_NAME = "cache";
 
+  // represents the concurrency level for the batch of requests we send to Momento
+  private static final int MAX_CONCURRENT_REQUESTS = 5;
+
+  private static final int TOTAL_KEYS = 20;
+
   public static void main(String[] args) {
 
     final CredentialProvider credentialProvider = new EnvVarCredentialProvider(API_KEY_ENV_VAR);
@@ -40,43 +46,61 @@ public class BatchUtilsExample {
         CacheClient.create(credentialProvider, Configurations.Laptop.latest(), DEFAULT_ITEM_TTL)) {
 
       createCache(client, CACHE_NAME);
+
       printStartBanner();
 
-      try (final MomentoBatchUtils momentoBatchUtils = MomentoBatchUtils.builder(client).build()) {
-        setupTestData(client, CACHE_NAME);
-        performBatchGet(momentoBatchUtils, CACHE_NAME);
-      }
+      setupTestData(client);
+      performBatchGet(client);
+
+      printEndBanner();
     }
   }
 
-  private static void setupTestData(CacheClient cacheClient, String cacheName) {
-    String[] keys = {"key1", "key2", "key3"};
-    String[] values = {"value1", "value2", "value3"};
-
-    for (int i = 0; i < keys.length; i++) {
-      cacheClient.set(cacheName, keys[i], values[i]).join();
-      System.out.println("Added " + keys[i] + " -> " + values[i]);
+  private static void setupTestData(CacheClient cacheClient) {
+    for (int i = 0; i < TOTAL_KEYS; i++) {
+      final String key = "key" + i;
+      final String value = "value" + i;
+      cacheClient.set(CACHE_NAME, key, value).join();
+      System.out.println("Added " + key + " -> " + value);
     }
   }
 
-  private static void performBatchGet(MomentoBatchUtils batchUtils, String cacheName) {
-    final BatchGetRequest.StringKeyBatchGetRequest request =
-        new BatchGetRequest.StringKeyBatchGetRequest(Arrays.asList("key1", "key2", "key3"));
+  private static void performBatchGet(final CacheClient client) {
 
-    System.out.println("Performing batchGet for key1, key2, key3");
-    final BatchGetResponse response = batchUtils.batchGet(cacheName, request).join();
+    // Building the util client using try-with-resources semantic to auto-close the client. Doing so
+    // shuts down
+    // and cleans up the executor
+    try (final MomentoBatchUtils momentoBatchUtils =
+        MomentoBatchUtils.builder(client)
+            .withMaxConcurrentRequests(MAX_CONCURRENT_REQUESTS)
+            .build()) {
 
-    if (response instanceof BatchGetResponse.StringKeyBatchGetSummary summary) {
-      for (BatchGetResponse.StringKeyBatchGetSummary.GetSummary getSummary :
-          summary.getSummaries()) {
-        System.out.println(
-            "Key: "
-                + getSummary.getKey()
-                + ", Value: "
-                + ((GetResponse.Hit) getSummary.getGetResponse()).valueString());
+      final List<String> keys = new ArrayList<>();
+
+      for (int i = 0; i < TOTAL_KEYS; i++) {
+        final String key = "key" + i;
+        keys.add(key);
       }
-    } else {
-      System.out.println("Error occurred during batch get operation.");
+
+      final BatchGetRequest.StringKeyBatchGetRequest request =
+          new BatchGetRequest.StringKeyBatchGetRequest(keys);
+
+      System.out.println("Performing batchGet for " + TOTAL_KEYS + " number of keys");
+
+      final BatchGetResponse response = momentoBatchUtils.batchGet(CACHE_NAME, request).join();
+
+      if (response instanceof BatchGetResponse.StringKeyBatchGetSummary summary) {
+        for (BatchGetResponse.StringKeyBatchGetSummary.GetSummary getSummary :
+            summary.getSummaries()) {
+          System.out.println(
+              "Key: "
+                  + getSummary.getKey()
+                  + ", Value: "
+                  + ((GetResponse.Hit) getSummary.getGetResponse()).valueString());
+        }
+      } else {
+        System.out.println("Error occurred during batch get operation.");
+      }
     }
   }
 
