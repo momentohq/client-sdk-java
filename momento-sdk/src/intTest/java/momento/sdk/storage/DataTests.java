f09843c8b5f6a7a75f58ed51ce477648d3c750f4
Michael Landis <malandis@gmail.com>
2024-07-09 13:08:37 -0700

    refactor: storage tests should use a single test store and base (#380)
    Refactors the storage tests to use a base class similar to the one
    just introduced for cache. Handles store set up and tear down uniformly.

--- momento-sdk/src/intTest/java/momento/sdk/storage/DataTests.java
+++ momento-sdk/src/intTest/java/momento/sdk/storage/DataTests.java
@@ -4,52 +4,26 @@ import static momento.sdk.TestUtils.randomString;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import momento.sdk.BaseTestClass;
-import momento.sdk.PreviewStorageClient;
-import momento.sdk.auth.CredentialProvider;
-import momento.sdk.config.StorageConfigurations;
+import momento.sdk.BaseStorageTestClass;
 import momento.sdk.exceptions.ClientSdkException;
 import momento.sdk.exceptions.StoreNotFoundException;
 import momento.sdk.responses.storage.DeleteResponse;
 import momento.sdk.responses.storage.GetResponse;
 import momento.sdk.responses.storage.PutResponse;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class DataTests extends BaseTestClass {
-  private static PreviewStorageClient client;
-
-  // TODO can set to the same value as the cache tests
-  // TODO rename env var for clarity to TEST_RESOURCE_NAME or similar
-  private final String storeName = System.getenv("TEST_CACHE_NAME");
-
-  @BeforeAll
-  static void setup() {
-    client =
-        new PreviewStorageClient(
-            CredentialProvider.fromEnvVar("MOMENTO_API_KEY"),
-            StorageConfigurations.Laptop.latest());
-
-    client.createStore(System.getenv("TEST_CACHE_NAME")).join();
-  }
-
-  @AfterAll
-  static void tearDown() {
-    client.close();
-  }
-
+public class DataTests extends BaseStorageTestClass {
   @Test
   void getReturnsValueAsStringAfterPut() {
     final String key = randomString("key");
     final String value = randomString("value");
 
     // Successful Set
-    final PutResponse putResponse = client.put(storeName, key, value).join();
+    final PutResponse putResponse = storageClient.put(storeName, key, value).join();
     assertThat(putResponse).isInstanceOf(PutResponse.Success.class);
 
     // Successful Get
-    final GetResponse getResponse = client.get(storeName, key).join();
+    final GetResponse getResponse = storageClient.get(storeName, key).join();
     assertThat(getResponse).isInstanceOf(GetResponse.Found.class);
     assertThat(getResponse.valueWhenFound().get().getString().get()).isEqualTo(value);
   }
@@ -60,11 +34,11 @@ public class DataTests extends BaseTestClass {
     final String value = randomString("value");
 
     // Successful Set
-    final PutResponse putResponse = client.put(storeName, key, value.getBytes()).join();
+    final PutResponse putResponse = storageClient.put(storeName, key, value.getBytes()).join();
     assertThat(putResponse).isInstanceOf(PutResponse.Success.class);
 
     // Successful Get
-    final GetResponse getResponse = client.get(storeName, key).join();
+    final GetResponse getResponse = storageClient.get(storeName, key).join();
     assertThat(getResponse).isInstanceOf(GetResponse.Found.class);
     assertThat(getResponse.valueWhenFound().get().getByteArray().get()).isEqualTo(value.getBytes());
   }
@@ -75,11 +49,11 @@ public class DataTests extends BaseTestClass {
     final long value = 42L;
 
     // Successful Set
-    final PutResponse putResponse = client.put(storeName, key, value).join();
+    final PutResponse putResponse = storageClient.put(storeName, key, value).join();
     assertThat(putResponse).isInstanceOf(PutResponse.Success.class);
 
     // Successful Get
-    final GetResponse getResponse = client.get(storeName, key).join();
+    final GetResponse getResponse = storageClient.get(storeName, key).join();
     assertThat(getResponse).isInstanceOf(GetResponse.Found.class);
     assertThat(getResponse.valueWhenFound().get().getLong().get()).isEqualTo(value);
   }
@@ -90,11 +64,11 @@ public class DataTests extends BaseTestClass {
     final double value = 3.14;
 
     // Successful Set
-    final PutResponse putResponse = client.put(storeName, key, value).join();
+    final PutResponse putResponse = storageClient.put(storeName, key, value).join();
     assertThat(putResponse).isInstanceOf(PutResponse.Success.class);
 
     // Successful Get
-    final GetResponse getResponse = client.get(storeName, key).join();
+    final GetResponse getResponse = storageClient.get(storeName, key).join();
     assertThat(getResponse).isInstanceOf(GetResponse.Found.class);
     assertThat(getResponse.valueWhenFound().get().getDouble().get()).isEqualTo(value);
   }
@@ -102,7 +76,7 @@ public class DataTests extends BaseTestClass {
   @Test
   void storeKeyNotFound() {
     // Get key that was not set
-    final GetResponse response = client.get(storeName, randomString("key")).join();
+    final GetResponse response = storageClient.get(storeName, randomString("key")).join();
     assertThat(response).isInstanceOf(GetResponse.NotFound.class);
     assert response.valueWhenFound().isEmpty();
     assert response instanceof GetResponse.NotFound;
@@ -113,11 +87,11 @@ public class DataTests extends BaseTestClass {
   public void badStoreNameReturnsError() {
     final String storeName = randomString("name");
 
-    final GetResponse getResponse = client.get(storeName, "").join();
+    final GetResponse getResponse = storageClient.get(storeName, "").join();
     assertThat(getResponse).isInstanceOf(GetResponse.Error.class);
     assertThat(((GetResponse.Error) getResponse)).hasCauseInstanceOf(StoreNotFoundException.class);
 
-    final PutResponse putResponse = client.put(storeName, "", "").join();
+    final PutResponse putResponse = storageClient.put(storeName, "", "").join();
     assertThat(putResponse).isInstanceOf(PutResponse.Error.class);
     assertThat(((PutResponse.Error) putResponse)).hasCauseInstanceOf(StoreNotFoundException.class);
   }
@@ -126,8 +100,8 @@ public class DataTests extends BaseTestClass {
   public void allowEmptyKeyValuesOnGet() throws Exception {
     final String emptyKey = "";
     final String emptyValue = "";
-    client.put(storeName, emptyKey, emptyValue).get();
-    final GetResponse response = client.get(storeName, emptyKey).get();
+    storageClient.put(storeName, emptyKey, emptyValue).get();
+    final GetResponse response = storageClient.get(storeName, emptyKey).get();
     assertThat(response).isInstanceOf(GetResponse.Found.class);
     assert response.valueWhenFound().get().getString().get().isEmpty();
   }
@@ -137,15 +111,15 @@ public class DataTests extends BaseTestClass {
     final String key = "key";
     final String value = "value";
 
-    client.put(storeName, key, value).get();
-    final GetResponse getResponse = client.get(storeName, key).get();
+    storageClient.put(storeName, key, value).get();
+    final GetResponse getResponse = storageClient.get(storeName, key).get();
     assertThat(getResponse).isInstanceOf(GetResponse.Found.class);
     assertThat(getResponse.valueWhenFound().get().getString().get()).isEqualTo(value);
 
-    final DeleteResponse deleteResponse = client.delete(storeName, key).get();
+    final DeleteResponse deleteResponse = storageClient.delete(storeName, key).get();
     assertThat(deleteResponse).isInstanceOf(DeleteResponse.Success.class);
 
-    final GetResponse getAfterDeleteResponse = client.get(storeName, key).get();
+    final GetResponse getAfterDeleteResponse = storageClient.get(storeName, key).get();
     assert getAfterDeleteResponse.valueWhenFound().isEmpty();
     assert getAfterDeleteResponse instanceof GetResponse.NotFound;
   }
@@ -154,7 +128,7 @@ public class DataTests extends BaseTestClass {
   public void deleteNonExistentKey() throws Exception {
     final String key = randomString("key");
 
-    final DeleteResponse deleteResponse = client.delete(storeName, key).get();
+    final DeleteResponse deleteResponse = storageClient.delete(storeName, key).get();
     assertThat(deleteResponse).isInstanceOf(DeleteResponse.Success.class);
   }
 }
