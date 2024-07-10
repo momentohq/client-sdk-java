58e0fa19232e5a161d299c11822955224aca53d1
Chris Price <chris@momentohq.com>
2024-07-10 16:46:10 -0700

    fix: debugging gradle docs ci tasks

--- examples/storage/src/main/java/momento/client/example/doc_examples/CheatSheet.java
+++ examples/storage/src/main/java/momento/client/example/doc_examples/CheatSheet.java
@@ -1,11 +1,24 @@
 package momento.client.example.doc_examples;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.joran.util.ConfigurationWatchListUtil;
 import momento.sdk.PreviewStorageClient;
 import momento.sdk.auth.CredentialProvider;
 import momento.sdk.config.StorageConfigurations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
 
 public class CheatSheet {
   public static void main(String[] args) {
+
+    final Logger logger = LoggerFactory.getLogger(CheatSheet.class);
+    LoggerContext loggerContext = ((ch.qos.logback.classic.Logger)logger).getLoggerContext();
+    URL mainURL = ConfigurationWatchListUtil.getMainWatchURL(loggerContext);
+    System.out.println(mainURL);
+    // or even
+    logger.info("Logback used '{}' as the configuration file.", mainURL);
     try (final var storageClient =
                  new PreviewStorageClient(
                          CredentialProvider.fromEnvVar("MOMENTO_API_KEY"),
