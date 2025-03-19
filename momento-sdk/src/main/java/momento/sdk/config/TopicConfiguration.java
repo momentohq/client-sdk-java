c00a1e0ab82ebe11202ef1c3a0f82b3391d82108
Nate Anderson <nate@momentohq.com>
2025-03-19 10:49:42 -0700

    chore: improve subscription reconnection logic
    Move the topic subscription reconnection logic into a retry strategy
    and eligibility strategy that can be configured by the client.
    
    Add the subscription retry strategy to TopicConfiguration. Add new
    constructors that take the retry strategy, and new ones that don't take
    a logger, which is only used inside the TopicConfiguration itself.
    
    Prevent potential race conditions that could occur when unsubscribing
    from a connection while it is reconnecting by checking if the user
    called unsubscribe before reconnecting, and by making the variables
    used in reconnection atomic.
    
    Call close in SubscriptionWrapper when a stream ends to stop a
    subscription from leaking threads from the retry executor service. We
    may want to share a thread pool between the subscriptions instead of
    making a new one per subscription, so that we don't need to make a new
    one each time. We could also use it to execute the callbacks, which are
    currently executed in the gRPC thread pool and could cause backpressure
    if they are long-running.
    
    Add empty default implementations for the test methods in
    IScsTopicConnection to clean up its use in the topic client.
    
    Remove SendSubscribeOptions, since we already have a class that contains
    the callbacks, and because it had an unused subscription object.
    
    Get rid of CancelableClientCallStreamObserver's ClientCallStreamObserver
    extension, because that class is meant for observing requests, not
    responses, and we only need a simple cancel method.
    
    Make TopicClientLocalTest's unrecoverable error 'not found', because the
    previous error is now recoverable.

--- momento-sdk/src/main/java/momento/sdk/config/TopicConfiguration.java
+++ momento-sdk/src/main/java/momento/sdk/config/TopicConfiguration.java
@@ -7,24 +7,35 @@ import javax.annotation.Nonnull;
 import momento.sdk.config.middleware.Middleware;
 import momento.sdk.config.transport.GrpcConfiguration;
 import momento.sdk.config.transport.TransportStrategy;
+import momento.sdk.retry.FixedDelaySubscriptionRetryStrategy;
+import momento.sdk.retry.SubscriptionRetryStrategy;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** The contract for SDK configurables. A configuration must have a transport strategy. */
 public class TopicConfiguration {
 
   private final TransportStrategy transportStrategy;
+  private final SubscriptionRetryStrategy subscriptionRetryStrategy;
   private final List<Middleware> middlewares;
   private final Logger logger;
 
   /**
-   * Creates a new topic configuration object.
+   * Creates a new topic configuration.
    *
    * @param transportStrategy Responsible for configuring network tunables.
+   * @param subscriptionRetryStrategy Responsible for determining when to reconnect a broken
+   *     subscription.
+   * @param middlewares List of middleware that can intercept and modify calls to Momento.
    * @param logger Responsible for logging
    */
   public TopicConfiguration(
-      TransportStrategy transportStrategy, List<Middleware> middlewares, Logger logger) {
+      TransportStrategy transportStrategy,
+      SubscriptionRetryStrategy subscriptionRetryStrategy,
+      List<Middleware> middlewares,
+      Logger logger) {
     this.transportStrategy = transportStrategy;
+    this.subscriptionRetryStrategy = subscriptionRetryStrategy;
     this.middlewares = middlewares;
     this.logger = logger;
   }
@@ -35,8 +46,68 @@ public class TopicConfiguration {
    * @param transportStrategy Responsible for configuring network tunables.
    * @param logger Responsible for logging
    */
+  public TopicConfiguration(
+      TransportStrategy transportStrategy, List<Middleware> middlewares, Logger logger) {
+    this(
+        transportStrategy,
+        new FixedDelaySubscriptionRetryStrategy(Duration.ofMillis(500)),
+        middlewares,
+        logger);
+  }
+
+  /**
+   * Creates a new topic configuration object.
+   *
+   * @param transportStrategy Responsible for configuring network tunables.
+   * @param middlewares List of middleware that can intercept and modify calls to Momento.
+   */
+  public TopicConfiguration(TransportStrategy transportStrategy, List<Middleware> middlewares) {
+    this(
+        transportStrategy,
+        new FixedDelaySubscriptionRetryStrategy(Duration.ofMillis(500)),
+        middlewares,
+        LoggerFactory.getLogger(TopicConfiguration.class));
+  }
+
+  /**
+   * Creates a new topic configuration object.
+   *
+   * @param transportStrategy Responsible for configuring network tunables.
+   * @param logger Responsible for logging
+   */
   public TopicConfiguration(TransportStrategy transportStrategy, Logger logger) {
     this(transportStrategy, new ArrayList<>(), logger);
+  }
+
+  /**
+   * Creates a new topic configuration object.
+   *
+   * @param transportStrategy Responsible for configuring network tunables.
+   */
+  public TopicConfiguration(TransportStrategy transportStrategy) {
+    this(transportStrategy, new ArrayList<>());
+  }
+
+  /**
+   * Configuration for subscription retries. By default, {@link
+   * momento.sdk.retry.FixedDelaySubscriptionRetryStrategy} gets used.
+   *
+   * @return The subscription retry strategy
+   */
+  public SubscriptionRetryStrategy getSubscriptionRetryStrategy() {
+    return subscriptionRetryStrategy;
+  }
+
+  /**
+   * Copy constructor that modifies the subscription retry strategy.
+   *
+   * @param subscriptionRetryStrategy The new subscription retry strategy
+   * @return a new TopicConfiguration with the updated subscription retry strategy
+   */
+  public TopicConfiguration withSubscriptionRetryStrategy(
+      @Nonnull final SubscriptionRetryStrategy subscriptionRetryStrategy) {
+    return new TopicConfiguration(
+        this.transportStrategy, subscriptionRetryStrategy, this.middlewares, this.logger);
   }
 
   /**
