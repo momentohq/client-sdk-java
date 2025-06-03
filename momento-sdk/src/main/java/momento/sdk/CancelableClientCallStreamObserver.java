0a5e7fdf3d300569650d25a05c4236d02b1ec23a
Nate Anderson <nate@momentohq.com>
2025-06-03 09:18:35 -0700

    chore: improve subscription reconnection logic (#435)
    Move the topic subscription reconnection logic into a retry strategy
    and eligibility strategy that can be configured by the client.
    
    Add the subscription retry strategy to TopicConfiguration. Add new
    constructors that take the retry strategy, and new ones that don't take
    a logger, which is only used inside the TopicConfiguration itself.
    
    Prevent potential race conditions that could occur when unsubscribing
    from a connection while it is reconnecting by checking if the user
    called unsubscribe before reconnecting, and by making the variables
    used in reconnection atomic.
    
    Call close in SubscriptionWrapper when a stream ends to stop a
    subscription from leaking threads from the retry executor service. We
    may want to share a thread pool between the subscriptions instead of
    making a new one per subscription, so that we don't need to make a new
    one each time. We could also use it to execute the callbacks, which are
    currently executed in the gRPC thread pool and could cause backpressure
    if they are long-running.
    
    Add empty default implementations for the test methods in
    IScsTopicConnection to clean up its use in the topic client.
    
    Remove SendSubscribeOptions, since we already have a class that contains
    the callbacks, and because it had an unused subscription object.
    
    Get rid of CancelableClientCallStreamObserver's ClientCallStreamObserver
    extension, because that class is meant for observing requests, not
    responses, and we only need a simple cancel method.
    
    Make TopicClientLocalTest's unrecoverable error 'not found', because the
    previous error is now recoverable.
    
    Unsubscribe should end with onCompleted instead of onError
    
    ---------
    
    Co-authored-by: anitarua <anita@momentohq.com>

--- momento-sdk/src/main/java/momento/sdk/CancelableClientCallStreamObserver.java
+++ momento-sdk/src/main/java/momento/sdk/CancelableClientCallStreamObserver.java
@@ -5,28 +5,10 @@ import io.grpc.stub.ClientResponseObserver;
 import javax.annotation.Nullable;
 
 public abstract class CancelableClientCallStreamObserver<TResp>
-    extends ClientCallStreamObserver<TResp> implements ClientResponseObserver<Object, TResp> {
+    implements ClientResponseObserver<Object, TResp> {
 
-  private ClientCallStreamObserver requestStream;
+  private ClientCallStreamObserver<Object> requestStream;
 
-  @Override
-  public boolean isReady() {
-    return false;
-  }
-
-  @Override
-  public void setOnReadyHandler(Runnable onReadyHandler) {}
-
-  @Override
-  public void request(int count) {}
-
-  @Override
-  public void setMessageCompression(boolean enable) {}
-
-  @Override
-  public void disableAutoInboundFlowControl() {}
-
-  @Override
   public void cancel(@Nullable String message, @Nullable Throwable cause) {
     if (requestStream != null) {
       requestStream.cancel(message, cause);
@@ -34,7 +16,7 @@ public abstract class CancelableClientCallStreamObserver<TResp>
   }
 
   @Override
-  public void beforeStart(ClientCallStreamObserver requestStream) {
+  public void beforeStart(ClientCallStreamObserver<Object> requestStream) {
     this.requestStream = requestStream;
   }
 }
--- momento-sdk/src/main/java/momento/sdk/SubscriptionWrapper.java
+++ momento-sdk/src/main/java/momento/sdk/SubscriptionWrapper.java
@@ -6,65 +6,103 @@ import grpc.cache_client.pubsub._SubscriptionRequest;
 import grpc.cache_client.pubsub._TopicItem;
 import grpc.cache_client.pubsub._TopicValue;
 import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import java.io.Closeable;
+import java.time.Duration;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import momento.sdk.exceptions.CacheServiceExceptionMapper;
 import momento.sdk.exceptions.InternalServerException;
 import momento.sdk.exceptions.TimeoutException;
+import momento.sdk.exceptions.UnknownException;
 import momento.sdk.internal.MomentoGrpcErrorDetails;
 import momento.sdk.internal.MomentoTransportErrorDetails;
+import momento.sdk.internal.SubscriptionState;
 import momento.sdk.responses.topic.TopicDiscontinuity;
 import momento.sdk.responses.topic.TopicMessage;
 import momento.sdk.responses.topic.TopicSubscribeResponse;
+import momento.sdk.retry.SubscriptionRetryStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class SubscriptionWrapper implements AutoCloseable {
-  private final Logger logger = LoggerFactory.getLogger(SubscriptionWrapper.class);
-  private final IScsTopicConnection connection;
-  private final SendSubscribeOptions options;
+class SubscriptionWrapper implements Closeable {
+  private static final Logger logger = LoggerFactory.getLogger(SubscriptionWrapper.class);
   private final long requestTimeoutSeconds;
-  private boolean firstMessage = true;
-  private boolean isConnectionLost = false;
+  private final IScsTopicConnection connection;
+  private final String cacheName;
+  private final String topicName;
+  private final ISubscriptionCallbacks callbacks;
+  private final SubscriptionState subscriptionState;
+  private final SubscriptionRetryStrategy retryStrategy;
+  private final AtomicBoolean firstMessage = new AtomicBoolean(true);
+  private final AtomicBoolean isConnectionLost = new AtomicBoolean(false);
+  private final AtomicBoolean isSubscribed = new AtomicBoolean(true);
 
+  // TODO: share a thread pool across subscription wrappers for retries and potentially for
+  // callbacks
   private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-  private CancelableClientCallStreamObserver<_SubscriptionItem> subscription;
-
-  private volatile CompletableFuture<Void> firstMessageTimeoutFuture = null;
+  private final AtomicReference<CancelableClientCallStreamObserver<_SubscriptionItem>>
+      subscription = new AtomicReference<>();
 
   SubscriptionWrapper(
-      IScsTopicConnection connection, SendSubscribeOptions options, long requestTimeoutSeconds) {
+      String cacheName,
+      String topicName,
+      IScsTopicConnection connection,
+      ISubscriptionCallbacks callbacks,
+      SubscriptionState subscriptionState,
+      long requestTimeoutSeconds,
+      SubscriptionRetryStrategy retryStrategy) {
+    this.cacheName = cacheName;
+    this.topicName = topicName;
     this.connection = connection;
-    this.options = options;
+    this.callbacks = callbacks;
+    this.subscriptionState = subscriptionState;
     this.requestTimeoutSeconds = requestTimeoutSeconds;
+    this.retryStrategy = retryStrategy;
   }
 
-  public CompletableFuture<Void> subscribeWithRetry() {
-    CompletableFuture<Void> future = new CompletableFuture<>();
-    firstMessageTimeoutFuture = new CompletableFuture<>();
+  /**
+   * This method returns a CompletableFuture that represents the asynchronous execution of the
+   * internal subscription logic with retry mechanism.
+   *
+   * @return A CompletableFuture representing the asynchronous execution of the internal
+   *     subscription logic with retry mechanism.
+   */
+  CompletableFuture<Void> subscribeWithRetry() {
+    final CompletableFuture<Void> future = new CompletableFuture<>();
+    final CompletableFuture<Void> firstMessageTimeoutFuture = new CompletableFuture<>();
+
+    // isSubscribed is true by default and is set to false only when unsubscribe is called.
+    // Do not allow resubscribe attempt on a subscription that is ending.
+    if (!isSubscribed.get()) {
+      completeExceptionally(
+          future, new UnknownException("Cannot subscribe to an unsubscribed subscription"));
+      return future;
+    }
 
     scheduler.schedule(
         () -> {
           if (!firstMessageTimeoutFuture.isDone()) {
             logger.warn(
-                "First message timeout exceeded for topic {} on cache {}",
-                options.getTopicName(),
-                options.getCacheName());
+                "First message timeout exceeded for topic {} on cache {}", topicName, cacheName);
 
             if (subscription != null) {
-              subscription.cancel("Timed out waiting for first message", null);
+              subscription.get().cancel("Timed out waiting for first message", null);
             }
 
             firstMessageTimeoutFuture.completeExceptionally(
                 new TimeoutException(
                     new RuntimeException(
                         "Timed out waiting for first message (heartbeat) for topic "
-                            + options.getTopicName()
+                            + topicName
                             + " on cache "
-                            + options.getCacheName()),
+                            + cacheName),
                     new MomentoTransportErrorDetails(
                         new MomentoGrpcErrorDetails(
                             Status.Code.DEADLINE_EXCEEDED,
@@ -75,7 +113,98 @@ class SubscriptionWrapper implements AutoCloseable {
         requestTimeoutSeconds,
         TimeUnit.SECONDS);
 
-    subscribeWithRetryInternal(future);
+    final CancelableClientCallStreamObserver<_SubscriptionItem> observer =
+        new CancelableClientCallStreamObserver<_SubscriptionItem>() {
+          @Override
+          public void onNext(_SubscriptionItem item) {
+            if (firstMessage.compareAndSet(true, false)) {
+              if (item.getKindCase() != _SubscriptionItem.KindCase.HEARTBEAT) {
+                completeExceptionally(
+                    future,
+                    new InternalServerException(
+                        "Expected heartbeat message for topic "
+                            + topicName
+                            + " on cache "
+                            + cacheName
+                            + ". Got: "
+                            + item.getKindCase()));
+              }
+              if (firstMessageTimeoutFuture != null) {
+                firstMessageTimeoutFuture.complete(null);
+              }
+              future.complete(null);
+              return;
+            }
+            if (isConnectionLost.compareAndSet(true, false)) {
+              callbacks.onConnectionRestored();
+            }
+            handleSubscriptionItem(item);
+          }
+
+          @Override
+          public void onError(Throwable t) {
+            if (firstMessage.get()) {
+              if (firstMessageTimeoutFuture != null && !firstMessageTimeoutFuture.isDone()) {
+                firstMessageTimeoutFuture.completeExceptionally(t);
+              }
+              if (!future.isDone()) {
+                future.completeExceptionally(t);
+              }
+              completeExceptionally(future, t);
+            } else {
+              logger.debug("Subscription failed, retrying...");
+              if (isConnectionLost.compareAndSet(false, true)) {
+                callbacks.onConnectionLost();
+              }
+
+              // If it was a CANCELLED error because unsubscribe was called, do not retry and
+              // exit gracefully.
+              if (t instanceof StatusRuntimeException) {
+                final StatusRuntimeException exception = (StatusRuntimeException) t;
+                if (exception.getStatus().getCode() == Status.Code.CANCELLED
+                    && exception.getMessage().contains("Unsubscribing")) {
+                  callbacks.onCompleted();
+                  close();
+                  return;
+                }
+              }
+
+              // Otherwise, determine if we should retry.
+              final Optional<Duration> retryOpt = retryStrategy.determineWhenToRetry(t);
+              if (retryOpt.isPresent()) {
+                if (isSubscribed.get()) {
+                  scheduleRetry(retryOpt.get(), () -> subscribeWithRetry());
+                } else {
+                  logger.debug("Cannot retry an unsubscribed subscription");
+                }
+              } else {
+                callbacks.onError(t);
+                close();
+              }
+            }
+          }
+
+          @Override
+          public void onCompleted() {
+            handleSubscriptionCompleted();
+          }
+        };
+
+    final _SubscriptionRequest subscriptionRequest =
+        _SubscriptionRequest.newBuilder()
+            .setCacheName(cacheName)
+            .setTopic(topicName)
+            .setResumeAtTopicSequenceNumber(subscriptionState.getResumeAtTopicSequenceNumber())
+            .setSequencePage(subscriptionState.getResumeAtTopicSequencePage())
+            .build();
+
+    try {
+      connection.subscribe(subscriptionRequest, observer);
+      subscriptionState.setSubscribed();
+    } catch (Exception e) {
+      completeExceptionally(future, e);
+    }
+    subscription.set(observer);
 
     // Combine the subscription future and the first-message timeout future.
     // Although CompletableFuture.anyOf(...) returns as soon as *either* future completes,
@@ -108,108 +237,21 @@ class SubscriptionWrapper implements AutoCloseable {
                 result.complete(null);
               }
             });
-
     return result;
   }
 
-  private void subscribeWithRetryInternal(CompletableFuture<Void> future) {
-    subscription =
-        new CancelableClientCallStreamObserver<_SubscriptionItem>() {
-          @Override
-          public void onNext(_SubscriptionItem item) {
-            if (firstMessage) {
-              firstMessage = false;
-              if (firstMessageTimeoutFuture != null) {
-                firstMessageTimeoutFuture.complete(null);
-              }
-
-              if (item.getKindCase() != _SubscriptionItem.KindCase.HEARTBEAT) {
-                future.completeExceptionally(
-                    new InternalServerException(
-                        "Expected heartbeat message for topic "
-                            + options.getTopicName()
-                            + " on cache "
-                            + options.getCacheName()
-                            + ". Got: "
-                            + item.getKindCase()));
-              }
-              future.complete(null);
-              return;
-            }
-            if (isConnectionLost) {
-              isConnectionLost = false;
-              options.onConnectionRestored();
-            }
-            handleSubscriptionItem(item);
-          }
-
-          @Override
-          public void onError(Throwable t) {
-            if (firstMessage) {
-              firstMessage = false;
-              if (firstMessageTimeoutFuture != null && !firstMessageTimeoutFuture.isDone()) {
-                firstMessageTimeoutFuture.completeExceptionally(t);
-              }
-              if (!future.isDone()) {
-                future.completeExceptionally(t);
-              }
-
-            } else {
-              logger.debug("Subscription failed, retrying...");
-              if (!isConnectionLost) {
-                isConnectionLost = true;
-                options.onConnectionLost();
-              }
-              if (t instanceof io.grpc.StatusRuntimeException) {
-                logger.debug(
-                    "Throwable is an instance of StatusRuntimeException, checking status code...");
-                io.grpc.StatusRuntimeException statusRuntimeException =
-                    (io.grpc.StatusRuntimeException) t;
-                if (statusRuntimeException.getStatus().getCode() == Status.Code.UNAVAILABLE) {
-                  logger.info("Status code is UNAVAILABLE, retrying subscription after a delay...");
-                  scheduleRetry(() -> subscribeWithRetryInternal(future));
-                } else {
-                  logger.debug("Status code is not UNAVAILABLE, not retrying subscription.");
-                  options.onError(t);
-                }
-              } else {
-                logger.debug(
-                    "Throwable is not an instance of StatusRuntimeException, not retrying subscription.");
-                options.onError(t);
-              }
-            }
-          }
-
-          @Override
-          public void onCompleted() {
-            handleSubscriptionCompleted();
-          }
-        };
-
-    _SubscriptionRequest subscriptionRequest =
-        _SubscriptionRequest.newBuilder()
-            .setCacheName(options.getCacheName())
-            .setTopic(options.getTopicName())
-            .setResumeAtTopicSequenceNumber(
-                options.subscriptionState.getResumeAtTopicSequenceNumber())
-            .setSequencePage(options.subscriptionState.getResumeAtTopicSequencePage())
-            .build();
-
-    try {
-      connection.subscribe(subscriptionRequest, subscription);
-      options.subscriptionState.setSubscribed();
-    } catch (Exception e) {
-      future.completeExceptionally(
-          new TopicSubscribeResponse.Error(CacheServiceExceptionMapper.convert(e)));
-    }
+  private void completeExceptionally(CompletableFuture<Void> future, Throwable t) {
+    future.completeExceptionally(
+        new TopicSubscribeResponse.Error(CacheServiceExceptionMapper.convert(t)));
+    close();
   }
 
-  private void scheduleRetry(Runnable retryAction) {
-    scheduler.schedule(retryAction, 500, TimeUnit.MILLISECONDS);
+  private void scheduleRetry(Duration retryDelay, Runnable retryAction) {
+    scheduler.schedule(retryAction, retryDelay.toMillis(), TimeUnit.MILLISECONDS);
   }
 
   private void handleSubscriptionCompleted() {
-    this.options.onCompleted();
+    callbacks.onCompleted();
   }
 
   private void handleSubscriptionItem(_SubscriptionItem item) {
@@ -232,16 +274,16 @@ class SubscriptionWrapper implements AutoCloseable {
   private void handleSubscriptionDiscontinuity(_SubscriptionItem discontinuityItem) {
     logger.trace(
         "discontinuity {}, {}, {}, {}, {}",
-        options.getCacheName(),
-        options.getTopicName(),
+        cacheName,
+        topicName,
         discontinuityItem.getDiscontinuity().getLastTopicSequence(),
         discontinuityItem.getDiscontinuity().getNewTopicSequence(),
         discontinuityItem.getDiscontinuity().getNewSequencePage());
-    options.subscriptionState.setResumeAtTopicSequenceNumber(
+    subscriptionState.setResumeAtTopicSequenceNumber(
         discontinuityItem.getDiscontinuity().getNewTopicSequence());
-    options.subscriptionState.setResumeAtTopicSequencePage(
+    subscriptionState.setResumeAtTopicSequencePage(
         discontinuityItem.getDiscontinuity().getNewSequencePage());
-    options.onDiscontinuity(
+    callbacks.onDiscontinuity(
         new TopicDiscontinuity(
             discontinuityItem.getDiscontinuity().getLastTopicSequence(),
             discontinuityItem.getDiscontinuity().getNewTopicSequence(),
@@ -249,32 +291,32 @@ class SubscriptionWrapper implements AutoCloseable {
   }
 
   private void handleSubscriptionHeartbeat() {
-    logger.trace("heartbeat {} {}", options.getCacheName(), options.getTopicName());
-    options.onHeartbeat();
+    logger.trace("heartbeat {} {}", cacheName, topicName);
+    callbacks.onHeartbeat();
   }
 
   private void handleSubscriptionUnknown() {
-    logger.warn("unknown {} {}", options.getCacheName(), options.getTopicName());
+    logger.warn("unknown {} {}", cacheName, topicName);
   }
 
   private void handleSubscriptionItemMessage(_SubscriptionItem item) {
     _TopicItem topicItem = item.getItem();
     _TopicValue topicValue = topicItem.getValue();
-    options.subscriptionState.setResumeAtTopicSequenceNumber(topicItem.getTopicSequenceNumber());
-    options.subscriptionState.setResumeAtTopicSequencePage(topicItem.getSequencePage());
+    subscriptionState.setResumeAtTopicSequenceNumber(topicItem.getTopicSequenceNumber());
+    subscriptionState.setResumeAtTopicSequencePage(topicItem.getSequencePage());
     TopicMessage message;
 
     switch (topicValue.getKindCase()) {
       case TEXT:
         message =
             handleSubscriptionTextMessage(topicValue.getText(), item.getItem().getPublisherId());
-        this.options.onItem(message);
+        callbacks.onItem(message);
         break;
       case BINARY:
         message =
             handleSubscriptionBinaryMessage(
                 topicValue.getBinary().toByteArray(), item.getItem().getPublisherId());
-        this.options.onItem(message);
+        callbacks.onItem(message);
         break;
       default:
         handleSubscriptionUnknown();
@@ -294,19 +336,16 @@ class SubscriptionWrapper implements AutoCloseable {
   }
 
   public void unsubscribe() {
-    subscription.cancel(
-        "Unsubscribing from topic: "
-            + options.getTopicName()
-            + " in cache: "
-            + options.getCacheName(),
-        null);
+    if (isSubscribed.compareAndSet(true, false)) {
+      subscription
+          .get()
+          .cancel("Unsubscribing from topic: " + topicName + " in cache: " + cacheName, null);
+      close();
+    }
   }
 
   @Override
   public void close() {
-    if (subscription != null) {
-      subscription.onCompleted();
-    }
     scheduler.shutdown();
   }
 }
