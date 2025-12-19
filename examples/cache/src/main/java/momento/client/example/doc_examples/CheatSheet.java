53d556811aa226ba43715642a495d315b48abea5
anitarua <anita@momentohq.com>
2025-12-19 15:52:59 -0800

    missed some examples, add docs snippets

--- examples/cache/src/main/java/momento/client/example/doc_examples/CheatSheet.java
+++ examples/cache/src/main/java/momento/client/example/doc_examples/CheatSheet.java
@@ -6,10 +6,11 @@ import momento.sdk.auth.CredentialProvider;
 import momento.sdk.config.Configurations;
 
 public class CheatSheet {
+
   public static void main(String[] args) {
     try (final CacheClient cacheClient =
         CacheClient.create(
-            CredentialProvider.fromEnvVar("MOMENTO_API_KEY"),
+            CredentialProvider.fromEnvVarV2(),
             Configurations.Laptop.v1(),
             Duration.ofSeconds(60) /* defaultTTL for your cache items*/,
             Duration.ofSeconds(10) /* eagerConnectionTimeout, default is 30 seconds */)) {
